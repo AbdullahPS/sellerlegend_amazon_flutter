fafba34f27b95b20e3cf907fb7de516ca4beba6c
AbdullahPS <abdullahtabbal@outlook.com>
2021-07-02 19:45:54 +0300

    Adjust height of cards to be smaller for Webview only(Mobile to be added later)

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,15 +4,19 @@ import 'package:sellerlegend/widgets/DayBox.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 4,
-          crossAxisSpacing: 15.0,
-          mainAxisSpacing: 15.0,
-        ),
-        itemCount: 4,
-        itemBuilder: (BuildContext context, int index) {
-          return Product();
-        });
+    return Scaffold(
+      body: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 4,
+            crossAxisSpacing: 15.0,
+            mainAxisSpacing: 15.0,
+            childAspectRatio: MediaQuery.of(context).size.width /
+                (MediaQuery.of(context).size.height),
+          ),
+          itemCount: 4,
+          itemBuilder: (BuildContext context, int index) {
+            return Product();
+          }),
+    );
   }
 }
--- lib/widgets/DayBox.dart
+++ lib/widgets/DayBox.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
 
 class Product extends StatefulWidget {
   const Product({Key? key}) : super(key: key);
@@ -11,115 +12,133 @@ class Product extends StatefulWidget {
 class _ProductState extends State<Product> {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      //padding: EdgeInsets.all(20),
-      child: Card(
-        semanticContainer: true,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-        elevation: 5,
-        child: Column(
-          children: <Widget>[
-            const ListTile(
+    return Card(
+      semanticContainer: true,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      elevation: 5,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Expanded(
+            child: ListTile(
               title: Text('Today'),
               subtitle: Text('von 1.1. bis 12.1'),
               trailing: Icon(Icons.list),
             ),
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Column(
+          ),
+          Expanded(
+            flex: 2,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      '100 000',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    Text('Ordered Product Sales'),
+                    Text(
+                      '100 000',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    Text('Revenue'),
+                    Text(
+                      '100 000',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    Text('Estimated Profit'),
+                  ],
+                ),
+                Expanded(
+                  child: Table(
+                    border: TableBorder(
+                        horizontalInside: BorderSide(
+                            width: 2,
+                            color: Colors.blue,
+                            style: BorderStyle.solid)),
                     children: [
-                      Text(
-                        '100 000',
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      Text('Ordered Product Sales'),
-                      Text(
-                        '100 000',
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      Text('Revenue'),
-                      Text(
-                        '100 000',
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      Text('Estimated Profit'),
-                    ],
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width / 8,
-                    padding: EdgeInsets.symmetric(vertical: 10.0),
-                    child: Table(
-                      border: TableBorder(
-                          horizontalInside: BorderSide(
-                              width: 1,
-                              color: Colors.blue,
-                              style: BorderStyle.solid)),
-                      children: [
-                        TableRow(children: [
-                          Column(children: [
-                            Text(
+                      TableRow(children: [
+                        Column(children: [
+                          Padding(
+                            child: Text(
                               'Orders',
                             ),
-                          ]),
-                          Text('b')
+                            padding: EdgeInsets.only(top: 3),
+                          ),
                         ]),
-                        TableRow(children: [
-                          Column(children: [
-                            Text(
+                        Text('b')
+                      ]),
+                      TableRow(children: [
+                        Column(children: [
+                          Padding(
+                            child: Text(
                               'Units',
                               textAlign: TextAlign.right,
                             ),
-                          ]),
-                          Text('b')
+                            padding: EdgeInsets.only(top: 3),
+                          ),
                         ]),
-                        TableRow(children: [
-                          Column(children: [
-                            Text(
+                        Text('b')
+                      ]),
+                      TableRow(children: [
+                        Column(children: [
+                          Padding(
+                            child: Text(
                               'Promo',
                               textAlign: TextAlign.right,
                             ),
-                          ]),
-                          Text('b')
+                            padding: EdgeInsets.only(top: 3),
+                          ),
                         ]),
-                        TableRow(children: [
-                          Column(children: [
-                            Text(
+                        Text('b')
+                      ]),
+                      TableRow(children: [
+                        Column(children: [
+                          Padding(
+                            child: Text(
                               'Refunds',
                               textAlign: TextAlign.right,
                             ),
-                          ]),
-                          Text('b')
+                            padding: EdgeInsets.only(top: 3),
+                          ),
                         ]),
-                        TableRow(children: [
-                          Column(children: [
-                            Text(
+                        Text('b')
+                      ]),
+                      TableRow(children: [
+                        Column(children: [
+                          Padding(
+                            child: Text(
                               'Net Margin',
                               textAlign: TextAlign.right,
                             ),
-                          ]),
-                          Text('b')
+                            padding: EdgeInsets.only(top: 3),
+                          ),
                         ]),
-                        TableRow(children: [
-                          Column(children: [
-                            Text(
+                        Text('b')
+                      ]),
+                      TableRow(children: [
+                        Column(children: [
+                          Padding(
+                            child: Text(
                               'ROI',
                               textAlign: TextAlign.right,
                             ),
-                          ]),
-                          Text('b')
+                            padding: EdgeInsets.only(top: 3),
+                          ),
                         ]),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            )
-          ],
-        ),
+                        Text('b')
+                      ]),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
