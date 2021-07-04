f0a4f8863ab084430887c5c379c6be5369b1a279
AbdullahPS <abdullahtabbal@outlook.com>
2021-07-04 12:37:19 +0300

    Homepage: Add empty Triple tab view

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -5,18 +5,53 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 4,
-            crossAxisSpacing: 15.0,
-            mainAxisSpacing: 15.0,
-            childAspectRatio: MediaQuery.of(context).size.width /
-                (MediaQuery.of(context).size.height),
+      body: Column(children: [
+        SizedBox(
+          height: 250,
+          child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 4,
+                crossAxisSpacing: 15.0,
+                mainAxisSpacing: 15.0,
+                childAspectRatio: MediaQuery.of(context).size.width /
+                    (MediaQuery.of(context).size.height),
+              ),
+              itemCount: 4,
+              itemBuilder: (BuildContext context, int index) {
+                return Product();
+              }),
+        ),
+        SizedBox(
+          height: MediaQuery.of(context).size.height - 250,
+          child: DefaultTabController(
+            length: 3,
+            child: Scaffold(
+              appBar: AppBar(
+                bottom: TabBar(
+                  tabs: [
+                    Tab(icon: Icon(Icons.directions_car)),
+                    Tab(icon: Icon(Icons.directions_transit)),
+                    Tab(icon: Icon(Icons.directions_bike)),
+                  ],
+                ),
+                title: Text('Sanawat Sellerlegend'),
+              ),
+              body: TabBarView(
+                children: [
+                  Center(child: Text('ALLE PRODUKTE')),
+                  Center(
+                    child: FlatButton(
+                      child: Text('Click here'),
+                      onPressed: Navigator.of(context).pushNamed('testpage'),
+                    ),
+                  ),
+                  Center(child: Text('TAB 3')),
+                ],
+              ),
+            ),
           ),
-          itemCount: 4,
-          itemBuilder: (BuildContext context, int index) {
-            return Product();
-          }),
+        ),
+      ]),
     );
   }
 }
