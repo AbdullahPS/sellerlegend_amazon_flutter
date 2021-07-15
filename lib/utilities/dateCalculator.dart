b0688351872bde804a4069c918f6498432dd9807
AbdullahPS <abdullahtabbal@outlook.com>
2021-07-16 02:16:46 +0300

    Datecalculator utility: Reformat and add missed Sunday to Dayslist

--- lib/utilities/dateCalculator.dart
+++ lib/utilities/dateCalculator.dart
@@ -17,22 +17,18 @@ String getWeekday(int weekNum) {
       return 'Sunday';
     case 2:
       return 'Monday';
-      break;
     case 3:
       return 'Tuesday';
-
     case 4:
       return 'Wednesday';
-
     case 5:
       return 'Thursday';
-
     case 6:
       return 'Friday';
-
     case 7:
       return 'Saturday';
-
+    case 8:
+      return 'Sunday';
     default:
       return 'something is wrong';
   }
