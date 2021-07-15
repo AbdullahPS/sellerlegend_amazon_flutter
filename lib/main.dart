9e94f1b8f4855e7e127d1766b63690f284170291
AbdullahPS <abdullahtabbal@outlook.com>
2021-07-16 01:44:49 +0300

    Change Appname

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() => runApp(WaterApp());
 class WaterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'WaterApp', home: Home(), routes: {
+    return MaterialApp(title: 'Sellerlegend', home: Home(), routes: {
       Home.routePathName: (ctx) => Home(),
       Test.routePathName: (ctx) => Test()
     });
